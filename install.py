e1f6b7cd105bdaeec8bef0fdbdfe154bd13fc3d1
oicleevan <oicleevan@protonmail.com>
2021-09-23 21:38:59 -0400

    Rewrite in Python

---
+++ install.py
@@ -0,0 +1,18 @@
+import os,sys
+
+version = "6.16-GE-1"
+
+url=f"https://github.com/GloriousEggroll/proton-ge-custom/releases/download/{version}/Proton-{version}.tar.gz"
+
+print("Installing the newest version of proton-ge\n")
+
+Commands = [
+    "wget " + url,
+    "mkdir -p ~/.steam/root/compatibilitytools.d/",
+    f"tar -xvf Proton-{version}.tar.gz -C ~/.steam/root/compatibilitytools.d/"
+]
+
+for i in Commands:
+    os.system(i)
+
+print(f"\nProton-GE {version} installed.")
--- installer.sh
+++
@@ -1,8 +0,0 @@
-echo "Downloading proton-ge-custom!"
-
-wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/6.16-GE-1/Proton-6.16-GE-1.tar.gz
-tar -xvf Proton-6.16-GE-1.tar.gz
-mkdir -p ~/.steam/root/compatibilitytools.d/
-cp -r Proton-6.16-GE-1/ ~/.steam/root/compatibilitytools.d/
-
-echo "Installation complete."
